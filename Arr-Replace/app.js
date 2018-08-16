cb6cf2534a72e980b95c8221f52ea0944b5332a1
kdabbir <d.kousthubhakumar@gmail.com>
2018-08-16 19:07:29 +0530

    Added array replace implementation

--- Arr-Replace/app.js
+++ Arr-Replace/app.js
@@ -1,6 +1,41 @@
 
-const input2 = [1,2,3,];
+const input2 = [1,2,3,4,0,5,6];
 
-const multiply
+const main = (numbers) => {
+  // one iteration to get product of whole array
 
-console.log(spiralOrder(input2));
+  const arrayProduct = numbers.reduce( (product, value) =>  product * (value === 0 ? 1 : value), 1);
+  // one iteration to map new array values by dividing 
+  // full array product by current value
+  return numbers.map( (value) => {
+    if(value ===0) return 0;
+    return arrayProduct / value
+  });
+}
+
+//ultimate braintwisting solution
+function ProductBT(arr){
+
+  var temp = [];
+
+
+  var product = 1;
+  for(var i=0; i<arr.length; i++){
+      temp[i] = product;
+      product *= arr[i];
+      // console.log(product);
+  }
+  // console.log(temp);
+
+  product = 1;
+  for(var i=arr.length-1; i>=0; i--){
+      temp[i] *= product;
+      product *= arr[i];
+     //console.log(product);
+     //console.log(temp);
+
+  }
+
+  return temp;
+}
+console.log(main(input2));
