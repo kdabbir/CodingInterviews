488f423e2379f2e422f586458f58c737dd09aed5
kdabbir <d.kousthubhakumar@gmail.com>
2018-08-26 17:49:36 +0530

    added babel, jest and eslint integration for testing before commits

--- src/Trees/Binary-Tree-Max-Depth/app.js
+++ src/Trees/Binary-Tree-Max-Depth/app.js
@@ -31,4 +31,3 @@ var maxDepth = function(root) {
 
 const tree = {"val":3,"right":{"val":20,"right":{"val":7,"right":null,"left":null},"left":{"val":15,"right":null,"left":null}},"left":{"val":9,"right":null,"left":null}};
 console.log(maxDepth(tree));
-
