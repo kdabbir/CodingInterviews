9aaa832e8b4fa837dd88c2f926251b6263588984
kdabbir <d.kousthubhakumar@gmail.com>
2019-08-13 20:43:57 +0530

    added tests for factorial

---
+++ src/Math-And-Tricky-Scenarios/Factorial/__test__/factorial.test.js
@@ -0,0 +1,9 @@
+import factorial from '../factorial';
+
+describe('factorial', () => {
+  it('should calculate factorial', () => {
+    expect(factorial(0)).toBe(1);
+    expect(factorial(1)).toBe(2);
+    expect(factorial(5)).toBe(720);
+  });
+});
