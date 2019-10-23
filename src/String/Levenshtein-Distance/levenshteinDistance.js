5f77a8e3b548a2e52b37fdbe568392e5880f310a
kkd0018@HOMEOFFICE.wal-mart.com <kkd0018@HOMEOFFICE.wal-mart.com>
2019-10-23 23:58:57 +0530

    adding parameters

--- src/String/Levenshtein-Distance/levenshteinDistance.js
+++ src/String/Levenshtein-Distance/levenshteinDistance.js
@@ -1,4 +1,8 @@
-
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
 export default function levenshteinDistance(a, b) {
   // Create empty edit distance matrix for all possible modifications of
   // substrings of a to substrings of b.
