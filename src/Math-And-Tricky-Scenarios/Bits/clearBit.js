cda93735381bb9e3685b43bfce4d328ef42ceae9
kkd0018@HOMEOFFICE.wal-mart.com <d.kousthubhakumar@gmail.com>
2019-10-29 23:52:09 +0530

    Adding params and readme

--- src/Math-And-Tricky-Scenarios/Bits/clearBit.js
+++ src/Math-And-Tricky-Scenarios/Bits/clearBit.js
@@ -1,4 +1,8 @@
-
+/**
+ * @param {number} number
+ * @param {number} bitPosition - zero based.
+ * @return {number}
+ */
 export default function clearBit(number, bitPosition) {
   const mask = ~(1 << bitPosition);
 
