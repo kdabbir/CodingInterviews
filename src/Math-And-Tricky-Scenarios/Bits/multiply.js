92cba9ef7712956c557a93e1c229a7a42974c565
kkd0018@HOMEOFFICE.wal-mart.com <d.kousthubhakumar@gmail.com>
2019-11-03 23:53:02 +0530

    adding params and return types

--- src/Math-And-Tricky-Scenarios/Bits/multiply.js
+++ src/Math-And-Tricky-Scenarios/Bits/multiply.js
@@ -3,6 +3,27 @@ import divideByTwo from './divideByTwo';
 import isEven from './isEven';
 import isPositive from './isPositive';
 
+/**
+ * Multiply two signed numbers using bitwise operations.
+ *
+ * If a is zero or b is zero or if both a and b are zeros:
+ * multiply(a, b) = 0
+ *
+ * If b is even:
+ * multiply(a, b) = multiply(2a, b/2)
+ *
+ * If b is odd and b is positive:
+ * multiply(a, b) = multiply(2a, (b-1)/2) + a
+ *
+ * If b is odd and b is negative:
+ * multiply(a, b) = multiply(2a, (b+1)/2) - a
+ *
+ * Time complexity: O(log b)
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
 export default function multiply(a, b) {
   // If a is zero or b is zero or if both a and b are zeros then the production is also zero.
   if (b === 0 || a === 0) {
