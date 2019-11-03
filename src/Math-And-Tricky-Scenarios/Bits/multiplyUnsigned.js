2f4eb636e8fbc8bd9a1282059cf7a1727ccd25d3
kkd0018@HOMEOFFICE.wal-mart.com <d.kousthubhakumar@gmail.com>
2019-11-03 23:54:45 +0530

    adding params and return types

--- src/Math-And-Tricky-Scenarios/Bits/multiplyUnsigned.js
+++ src/Math-And-Tricky-Scenarios/Bits/multiplyUnsigned.js
@@ -1,4 +1,21 @@
-
+/**
+ * Multiply to unsigned numbers using bitwise operator.
+ *
+ * The main idea of bitwise multiplication is that every number may be split
+ * to the sum of powers of two:
+ *
+ * I.e. 19 = 2^4 + 2^1 + 2^0
+ *
+ * Then multiplying number x by 19 is equivalent of:
+ *
+ * x * 19 = x * 2^4 + x * 2^1 + x * 2^0
+ *
+ * Now we need to remember that (x * 2^4) is equivalent of shifting x left by 4 bits (x << 4).
+ *
+ * @param {number} number1
+ * @param {number} number2
+ * @return {number}
+ */
 export default function multiplyUnsigned(number1, number2) {
   let result = 0;
 
