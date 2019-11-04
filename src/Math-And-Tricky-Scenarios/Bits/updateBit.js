18138b562655be53c2742c0535a7e38542ed07b1
kkd0018@HOMEOFFICE.wal-mart.com <d.kousthubhakumar@gmail.com>
2019-11-05 00:22:14 +0530

    Added parameters and return types

--- src/Math-And-Tricky-Scenarios/Bits/updateBit.js
+++ src/Math-And-Tricky-Scenarios/Bits/updateBit.js
@@ -1,4 +1,9 @@
-
+/**
+ * @param {number} number
+ * @param {number} bitPosition - zero based.
+ * @param {number} bitValue - 0 or 1.
+ * @return {number}
+ */
 export default function updateBit(number, bitPosition, bitValue) {
   // Normalized bit value.
   const bitValueNormalized = bitValue ? 1 : 0;
