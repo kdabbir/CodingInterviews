c23f9bab47f0f032c4b209e7cfe9ad713dd86387
kkd0018@HOMEOFFICE.wal-mart.com <d.kousthubhakumar@gmail.com>
2019-11-07 21:18:11 +0530

    adding description and params

--- src/Math-And-Tricky-Scenarios/Fibonacci/fibonacciNthClosedForm.js
+++ src/Math-And-Tricky-Scenarios/Fibonacci/fibonacciNthClosedForm.js
@@ -1,3 +1,10 @@
+/**
+ * Calculate fibonacci number at specific position using closed form function (Binet's formula).
+ * @see: https://en.wikipedia.org/wiki/Fibonacci_number#Closed-form_expression
+ *
+ * @param {number} position - Position number of fibonacci sequence (must be number from 1 to 75).
+ * @return {number}
+ */
 export default function fibonacciClosedForm(position) {
   const topMaxValidPosition = 70;
 
