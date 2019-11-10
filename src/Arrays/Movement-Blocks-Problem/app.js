f0d0b0bcdf7f907a261caf3d9d8d95583c3d2032
Kousthubha <d.kousthubhakumar@gmail.com>
2019-11-10 21:24:33 +0530

    Adding problem statement and inputs

--- src/Arrays/Movement-Blocks-Problem/app.js
+++ src/Arrays/Movement-Blocks-Problem/app.js
@@ -1,4 +1,28 @@
+// output the number of blocks at each position in hexadecimal
 
+// The machine understands the commands 'P','M', and 'L':
+
+// P; pickup from the pallet and and move to position 0
+
+// M; Move to the next position
+
+// L; Lower the block
+
+// Sample Input 1
+
+// String = "PMLPMMMLPMLPMML"
+
+// Sample Output 1
+
+// "0211000000"
+
+// Sample Input 2
+
+// "PLPLPLPLPLPLPLPLPLPL"
+
+// Sample Output 2
+
+// "A000000000"
 
 function initiateMovement(command) {
 	let blocks = new Array(10).fill(0);
