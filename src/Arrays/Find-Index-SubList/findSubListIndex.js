da3dd79d6648b2e44e797ad59cff863e7f12b456
Kousthubha <d.kousthubhakumar@gmail.com>
2019-11-10 18:38:44 +0530

    Adding problem statement and params

--- src/Arrays/Find-Index-SubList/findSubListIndex.js
+++ src/Arrays/Find-Index-SubList/findSubListIndex.js
@@ -1,4 +1,19 @@
 
+
+// Implement a method 'find' that will find the starting index (zero based) where the second list occurs as a sub-list in the first list. 
+// It should return -1 if the sub-list cannot be found. Arguments are always given, not empty. 
+// Sample Input 1 list1 = (1, 2, 3) list2 = (2, 3) 
+// Sample Output 1 1 Explanation As second list (2, 3) is sub-list in first list (1, 2, 3) at index 1 
+// Sample Input 2 list1 = (1, 2, 3) list2 = (3, 2) 
+// Sample Output 2 -1
+
+ /**
+  * 
+  * @param {LinkedList} list 
+  * @param {LinkedList} subList 
+  * @return {string}
+  */
+
 var findSubListIndex = function(list, subList) {
     let listAsString = linkedListToString(list);
     let subList = linkedListToString(subList);
