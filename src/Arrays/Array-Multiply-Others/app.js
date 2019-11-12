96497a527ea84cf0c43d603237782a2d6d197e2a
Kousthubha <d.kousthubhakumar@gmail.com>
2019-11-12 23:43:01 +0530

    Modifying logic for edge case

--- src/Arrays/Array-Multiply-Others/app.js
+++ src/Arrays/Array-Multiply-Others/app.js
@@ -1,3 +1,4 @@
+// To modify array such that each element is multiplied with other values.
 
 const input2 = [1,2,3,4,0,5,6];
 
@@ -13,29 +14,33 @@ const main = (numbers) => {
   });
 }
 
-//ultimate braintwisting solution
+// Multiplying from left and right of array
 function ProductBT(arr){
 
   var temp = [];
 
-
   var product = 1;
   for(var i=0; i<arr.length; i++){
+    if(arr[i]!=0){
       temp[i] = product;
       product *= arr[i];
       // console.log(product);
+    } else { 
+      temp[i] = 0;
+    }
   }
   // console.log(temp);
 
   product = 1;
   for(var i=arr.length-1; i>=0; i--){
+    if(arr[i]!=0){
       temp[i] *= product;
       product *= arr[i];
      //console.log(product);
      //console.log(temp);
-
+    }
   }
 
   return temp;
 }
-console.log(main(input2));
+console.log(ProductBT(input2));
